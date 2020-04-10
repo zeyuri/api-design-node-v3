66b37ae460e15ad32f1ef3b8b42fcbbe45f3c92d
Zé Yuri <jozeyuri@gmail.com>
2020-04-10 15:00:22 -0300

    my configs

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 
 export const app = express()
 
+const router = express.Router()
+
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,4 +14,28 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// dogs
+
+router
+  .route('/dog')
+  .get((req, res) => {
+    console.log(req)
+    res.send({ name: 'deno' })
+  })
+  .post()
+
+app.use(router)
+
+app.get('/data', (req, res) => {
+  res.send({ data: [1, 2, 3] })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(4200, () => {
+    console.log('server is on 4:200')
+  })
+}
